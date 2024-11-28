bb2f79fee03eec025654535766c68551bff95d56
akshaykh123 <87373738+akshaykh123@users.noreply.github.com>
2024-11-28 20:19:59 +0530

    made changes in app-js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React deployment
         </a>
       </header>
     </div>
